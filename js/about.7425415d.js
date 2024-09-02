f00cca6af618ac47510c81fb296c7578fdec9b0b
gevorg1990 <gevorg_gasparyan_2018@mail.ru>
2024-09-02 09:16:00 +0400

    Updates

---
+++ js/about.7425415d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23");const e={class:"about"},o=Object(c["i"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(c["B"])(),Object(c["h"])("div",e,s)}var i=a("d959"),b=a.n(i);const r={},d=b()(r,[["render",u]]);t["default"]=d}}]);
+//# sourceMappingURL=about.7425415d.js.map
